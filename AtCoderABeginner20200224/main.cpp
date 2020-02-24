14206ff292c30244b2c21ccbcadf5192edcebd13
ohsuyuki <papu8osu@gmail.com>
2020-02-24 17:08:27 +0900

    initial commit

--- AtCoderABeginner20200224/main.cpp
+++ AtCoderABeginner20200224/main.cpp
@@ -8,8 +8,22 @@
 
 #include <iostream>
 
+int getPrivateRating(int K, int R) {
+    if(K >= 10) {
+        return R;
+    } else {
+        return R + (100 * (10 - K));
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int N(0), R(0);
+
+    std::cin >> N >> R;
+
+    int PrivateRating = getPrivateRating(N, R);
+
+    std::cout << PrivateRating;
+
     return 0;
 }
